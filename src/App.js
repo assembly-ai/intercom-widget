1c97465b189bd221977d3d73641e1892bee77639
assembly-deepali <deepali@askassembly.app>
2024-10-03 04:13:00 -0700

    modify user

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Intercom from "@intercom/messenger-js-sdk";
 function App() {
     Intercom({
         app_id: "ei8bp2b2",
-        user_id: "deepali11221", // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
-        name: "deepali", // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
-        email: "deepali@askassembly.app", // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+        user_id: "sarah2121", // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+        name: "sarah", // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+        email: "sarah@askassembly.app", // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
         created_at: 1704067200, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
     });
 
