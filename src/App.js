bd7d718c15830a17b7d4117538a118b0b2a00403
assembly-deepali <deepali@askassembly.app>
2024-10-03 00:29:37 -0700

    push to intercom test widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// import Intercom from "@intercom/messenger-js-sdk";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Intercom({
+    // 	app_id: "ei8bp2b2",
+    // 	user_id: "deepali11221", // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+    // 	name: "deepali", // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+    // 	email: "deepali@askassembly.app", // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+    // 	created_at: 1704067200, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
+    // });
+
+	evaluationClient, _ := flipt.NewClient(
+		flipt.WithURL("https://<environment>-<organization>.flipt.cloud"),
+		flipt.WithClientTokenAuthentication(<api-key>),
+		flipt.WithNamespace(<namespace>)
+	)
+    return <div className="App">my app</div>;
 }
 
 export default App;
